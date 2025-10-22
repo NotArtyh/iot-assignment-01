0c4fd0644ca215ce886182d49bbfe8877d7a4bfa
Arthur Muller <arty97muller@gmail.com>
2025-10-22 12:08:32 +0200

    add config file definition
    moved defnition logic to config.h

--- assignment-01.ino
+++ assignment-01.ino
@@ -1,17 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
-
-#define L1 13
-#define L2 12
-#define L3 11
-#define L4 10
-#define LS 3
-#define B1 9
-#define B2 8
-#define B3 7
-#define B4 6
-#define POT A0
+#include <config.h>
 
 typedef enum
 {
---
+++ config.h
@@ -0,0 +1,17 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define L1 13
+#define L2 12
+#define L3 11
+#define L4 10
+#define LS 3
+
+#define B1 9
+#define B2 8
+#define B3 7
+#define B4 6
+
+#define POT A0
+
+#endif
