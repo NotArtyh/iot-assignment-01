f12d99a8f6e33fb9bf601a50eec5400be0beb19f
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2025-10-22 11:04:44 +0200

    Add fading led function

---
+++ assignment-01.ino
@@ -0,0 +1,82 @@
+#include <LiquidCrystal_I2C.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+#define L1 13
+#define L2 12
+#define L3 11
+#define L4 10
+#define LS 3
+#define B1 9
+#define B2 8
+#define B3 7
+#define B4 6
+#define POT A0
+
+typedef enum
+{
+  ST_INITIAL,
+  ST_SLEEP,
+  ST_SETUP,
+  ST_ROUND,
+  ST_LSOE,
+  ST_WIN,
+} State;
+
+// FADING LED VARIABLES
+int fadeAmount;
+int currIntensity;
+
+// start first state (trovare un modo per non dichiararlo globalmente)
+State currentState = ST_INITIAL;
+
+void setup()
+{
+
+  // LS SETUP
+  currIntensity = 0;
+  fadeAmount = 5;
+  pinMode(LS, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  switch (currentState)
+  {
+  case ST_INITIAL:
+    handle_initial_state();
+    fading_ls();
+
+    // transiton
+    break;
+  case ST_SLEEP:
+
+    break;
+  default:
+    break;
+  }
+}
+
+void handle_initial_state()
+{
+  // In the initial state, all green leds should be off but led LS that pulses (fading in and out),
+  // waiting for some player to start the game. The LCD should display the message “Welcome to TOS! Press B1 to Start” (on multiple lines).
+
+  display_message_lcd("Welcome to TOS! Press B1 to Start");
+}
+
+void display_message_lcd(char message[])
+{
+}
+
+void fading_ls()
+{
+  analogWrite(LS, currIntensity);
+  currIntensity = currIntensity + fadeAmount;
+  if (currIntensity == 0 || currIntensity == 255)
+  {
+    fadeAmount = -fadeAmount;
+  }
+  delay(20);
+}
