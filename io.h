ba48e5f97b592d82346e99bef8917fce3e59ecd6
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2025-10-24 11:39:06 +0200

    Add all functions to header file

--- io.h
+++ io.h
@@ -1,4 +1,15 @@
 #ifndef __IO__
 #define __IO__
 
+void setupGreenLeds();
+void setupLs();
+void setupButtons();
+void setupDisplay();
+void setupIO();
+
+void greenLedsOff();
+void pulseLs();
+
+void displayMessage(char message[]);
+
 #endif
