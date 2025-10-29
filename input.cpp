65d7378ed8fbef64480aeba0e59e4e917d6f97ee
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2025-10-29 10:38:59 +0100

    Moved setup functions to setup.cpp

--- input.cpp
+++ input.cpp
@@ -11,47 +11,6 @@ bool buttonPressed[NUM_BUTTONS] = {false, false, false, false};
 
 long lastButtonPressedTimestamps[NUM_BUTTONS];
 
-// initialize display
-
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
-
-// SETUP DEI LED, BUTTONS E DISPLAY (spostare tutto il setup altrove intanto lo lascio qui)
-
-void setupGreenLeds()
-{
-  for (int i = 0; i < NUM_GREEN_LEDS; i++)
-  {
-    pinMode(greenLeds[i], OUTPUT);
-  }
-}
-
-void setupLs()
-{
-  pinMode(LS, OUTPUT);
-}
-
-void setupButtons()
-{
-  for (int i = 0; i < NUM_BUTTONS; i++)
-  {
-    pinMode(inputButtons[i], INPUT);
-  }
-}
-
-void setupDisplay()
-{
-  lcd.init();
-  lcd.backlight();
-}
-
-void setupIO()
-{
-  setupGreenLeds();
-  setupLs();
-  setupButtons();
-  setupDisplay();
-}
-
 void buttonHandler(int i);
 void buttonHandler0() { buttonHandler(0); }
 void buttonHandler1() { buttonHandler(1); }
