a3bbf2f2db78a0d204e1d2628cdcde3f92f6d18d
Arthur Muller <arty97muller@gmail.com>
2025-11-28 19:28:48 +0100

    Remove pot comments

--- src/potentiometer/potentiometer.cpp
+++ src/potentiometer/potentiometer.cpp
@@ -1,15 +1,13 @@
 #include "config.h"
 #include <Arduino.h>
 
-int factor = 1; // mu serve globalmente
+int factor = 1;
 
 void pot_init() { pinMode(POT, INPUT); }
 
-// Aggiorna la difficoltà leggendo il potenziometro
 void pot_update_factor() {
     int potValue = analogRead(POT);
     factor = map(potValue, 0, 1023, 1, 4);
 }
 
-// Restituisce il valore attuale della difficoltà
 int pot_get_factor() { return factor; }
