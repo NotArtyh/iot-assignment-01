32df6d3ae99e90586cd75cfefd265a148c0ebeb6
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2025-11-28 19:58:50 +0100

    Add signature

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
+/****************************************************************************/
+/*
+Course: IoT Embedded Systems
+Year: 2025/2026
+Title: assignment-01
+Authors:
+    - Muller Arthur
+        Matricola:
+    - Cattolico Giuseppe
+        Matricola: 0001124318
+*/
+/****************************************************************************/
+
 #include "io.h"
 #include "state/fsm.h"
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
     fsm_init();
     input_init();
     output_init();
